05c956a8874a949371aae1f3a47c2bc251605ce1
WayneKim92 <eunchan0817@gamil.com>
2024-02-18 18:51:10 +0900

    refactor: switch 문으로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,48 @@
 export function smart(...args: any[]) {
   let output = '';
   for (const arg of args) {
-    if (typeof arg === 'object') {
-      if (arg === null) {
-        output += 'null ';
-      } else if (arg instanceof Date) {
-        output += arg.toISOString().trim() + ' '; // Date 객체 처리
-      } else if (arg instanceof RegExp) {
-        output += arg.toString() + ' '; // RegExp 객체 처리
-      } else if (Array.isArray(arg)) {
-        console.table(arg); // 2차원 배열 이상일 경우 console.table을 사용하여 출력
-      } else {
-        output += JSON.stringify(arg, null, 2) + ' ';
-      }
-      output += '\n'; // 객체가 전달되면 한 줄 개행 추가
-    } else if (typeof arg === 'function') {
-      // 함수를 문자열로 변환하고, 각 줄의 시작 부분에서 공백 문자를 제거
-      let funcStr = arg.toString().replace(/^ +/gm, '\t');
-      // 마지막 줄의 시작 부분에서 탭 문자를 제거
-      funcStr = funcStr.replace(/\t+}$/, '}');
-      output += funcStr + '\n';
-    } else if (typeof arg === 'undefined') {
-      output += 'undefined\n';
-    } else if (
-      typeof arg === 'string' &&
-      (arg === 'true' || arg === 'false' || 'undefined' || 'null')
-    ) {
-      output += `"${arg}"\n`; // 'true' 또는 'false' 문자열 처리
-    } else {
-      output += arg + '\n';
+    switch (typeof arg) {
+      case 'object':
+        if (arg === null) {
+          output += 'null ';
+        } else if (arg instanceof Date) {
+          output += arg.toISOString().trim() + ' ';
+        } else if (arg instanceof RegExp) {
+          output += arg.toString() + ' ';
+        } else if (Array.isArray(arg)) {
+          console.table(arg);
+        } else {
+          output += JSON.stringify(arg, null, 2) + ' ';
+        }
+        output += '\n';
+        break;
+
+      case 'function':
+        let funcStr = arg.toString().replace(/^ +/gm, '\t');
+        funcStr = funcStr.replace(/\t+}$/, '}');
+        output += funcStr + '\n';
+        break;
+
+      case 'undefined':
+        output += 'undefined\n';
+        break;
+
+      case 'string':
+        if (
+          arg === 'true' ||
+          arg === 'false' ||
+          arg === 'undefined' ||
+          arg === 'null'
+        ) {
+          output += `"${arg}"\n`;
+        } else {
+          output += arg + '\n';
+        }
+        break;
+
+      default:
+        output += arg + '\n';
+        break;
     }
   }
   console.log(output);
