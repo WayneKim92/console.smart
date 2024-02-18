b5608bd1e18a57f16e57390e4dc7a2d83571c50d
WayneKim92 <eunchan0817@gamil.com>
2024-02-18 18:24:48 +0900

    feat: 객체와 함수를 보기 좋게 출력하기

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,28 @@
+import 'console.smart';
 import * as React from 'react';
-
 import { StyleSheet, View, Text } from 'react-native';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    const data = {
+      name: 'John Doe',
+      age: 30,
+      isMarried: false,
+      hobbies: ['reading', 'gaming', 'coding'],
+      address: {
+        city: 'Seoul',
+        country: 'South Korea',
+      },
+      sayHello: function() {
+        return 'Hello, world!';
+      },
+    };
+
+    console.smart('Hello', 'world', 123, true, null, undefined, data, [1, 2, 3], data.sayHello);
+
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open the console</Text>
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,31 @@ export function smart(...args: any[]) {
   let output = '';
   for (const arg of args) {
     if (typeof arg === 'object') {
-      output += JSON.stringify(arg, null, 2) + ' ';
+      output += '\n'
+      if (arg === null) {
+        output += 'null ';
+      } else if (Array.isArray(arg)) {
+        output += '[ ' + arg.map(item => JSON.stringify(item, null, 2)).join(', ') + ' ] ';
+      } else {
+        output += JSON.stringify(arg, null, 2) + ' ';
+      }
+      output += '\n'; // 객체가 전달되면 한 줄 개행 추가
+    } else if (typeof arg === 'function') {
+      // 함수를 문자열로 변환하고, 각 줄의 시작 부분에서 공백 문자를 제거
+      let funcStr = arg.toString().replace(/^ +/gm, '\t');
+      // 마지막 줄의 시작 부분에서 탭 문자를 제거
+      funcStr = funcStr.replace(/\t+}$/, '}');
+      output += funcStr + ' ';
+    } else if (typeof arg === 'undefined') {
+      output += 'undefined ';
     } else {
       output += arg + ' ';
     }
   }
   console.log(output);
 }
+
+console['smart'] = smart;
 
 console.log(`
                            _                                     _
