59f077d4a1bcd0c6a9ff17e10ecbb72e7890f878
WayneKim92 <eunchan0817@gamil.com>
2024-02-18 22:49:52 +0900

    fix: null도 object인 점 대비

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,52 @@
 import { Platform } from 'react-native';
 
 let separator = ' ';
+
 function smart(...args: any[]) {
   let output = '';
-  for (const arg of args) {
-    switch (typeof arg) {
-      case 'object':
-        if (arg === null) {
-          output += 'null ';
-        } else if (arg instanceof Date) {
-          output += arg.toISOString().trim() + ' ';
-        } else if (arg instanceof RegExp) {
-          output += arg.toString() + ' ';
-        } else if (Array.isArray(arg)) {
-          console.table(arg);
-        } else {
-          output += JSON.stringify(arg, null, 2) + ' ';
-        }
-        output += separator;
-        break;
 
-      case 'function':
-        let funcStr = arg.toString().replace(/^ +/gm, '\t');
-        funcStr = funcStr.replace(/\t+}$/, '}');
-        output += funcStr + separator;
-        break;
+  try {
+    for (const arg of args) {
+      switch (typeof arg) {
+        case 'object':
+          if (arg === null) {
+            output += 'null ';
+          } else if (arg instanceof Date) {
+            output += arg.toISOString().trim() + ' ';
+          } else if (arg instanceof RegExp) {
+            output += arg.toString() + ' ';
+          } else if (Array.isArray(arg)) {
+            console.table(arg);
+          } else {
+            output += JSON.stringify(arg, null, 2) + ' ';
+          }
+          output += separator;
+          break;
 
-      case 'string':
-        output += `"${arg}"${separator}`;
-        break;
+        case 'function':
+          let funcStr = arg.toString().replace(/^ +/gm, '\t');
+          funcStr = funcStr.replace(/\t+}$/, '}');
+          output += funcStr + separator;
+          break;
 
-      case 'symbol':
-        output += arg.toString() + separator;
-        break;
+        case 'string':
+          output += `"${arg}"${separator}`;
+          break;
 
-      default:
-        output += arg + separator;
-        break;
+        case 'symbol':
+          output += arg.toString() + separator;
+          break;
+
+        default:
+          output += arg + separator;
+          break;
+      }
     }
+  } catch (e) {
+    output = args.toString();
+  } finally {
+    console.log(output);
   }
-  console.log(output);
 }
 
 // https://gist.github.com/samuthekid/6dbfee2ef6029d544a7d873224d53e16
@@ -52,7 +59,7 @@ if (Platform.OS !== 'web') {
         console.table(item, spc + tab, `${pre}[${i}]`);
       });
       console.log(' '.repeat(spc), '],');
-    } else if (typeof data === 'object') {
+    } else if (typeof data === 'object' && data !== null) {
       console.log(' '.repeat(spc), pre, '{');
       Object.entries(data).forEach(([key, value]) => {
         console.table(value, spc + tab, `${pre}${pre ? '.' : ''}${key}`);
